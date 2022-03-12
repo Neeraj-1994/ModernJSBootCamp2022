65795a78b1e486b0f1d4722e2f7d3584eafa3709
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2022-03-12 17:30:25 +0600

    simple number operations

---
+++ app.js
@@ -0,0 +1,22 @@
+// Introducing Numbers
+
+// Addition
+console.log(50 + 5);
+
+// Subtraction
+console.log(90 - 1);
+
+// Multiplication
+console.log(11111 * 7);
+
+// Division
+console.log(400 / 25);
+
+// Modulo
+console.log(27 % 2); // Gives the remainder
+
+// Exponents
+console.log(8 ** 3); // same as 8^3
+
+// Order of Operations
+console.log((4 - 7) * 3); // Follows PEMDAS
