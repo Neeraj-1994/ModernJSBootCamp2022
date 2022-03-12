0d17d84aa558055c23921889bdd739563bd6f483
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2022-03-12 17:53:22 +0600

    variable naming and syntax

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ console.log(8 ** 3); // same as 8^3
 console.log((4 - 7) * 3); // Follows PEMDAS
 */
 
-
+/*
 // NaN & Infinity
 
 // Ways to arrive to NaN
@@ -33,3 +33,20 @@ console.log(1 + NaN);
 // Ways to arrive to Infinity & -Infinity
 console.log(1/0);
 console.log(-1/0);
+*/
+
+
+// Variables & let
+
+// Basic Syntax
+let someName = 'value';
+let age = 72;
+
+// Recalling variables
+let hens = 4;
+let rooster = 2;
+console.log(hens + rooster);
+
+// Rules of naming variables
+let numOfHens = 4;
+let averageRating = 9.7;
