0f3c8cc579ab457eee27a7fa53168dbdc2cbf95c
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2022-03-18 13:31:15 +0600

    accessing array elements and properties

--- array1.js
+++ array1.js
@@ -1,3 +1,4 @@
+/*
 // Capture Collections of Data with Arrays
 
 // Creating Arrays
@@ -9,3 +10,18 @@ let lottoNumbers = [45, 12, 23, 25, 34];
 let myCollection = ['dog', 12, NaN, true, null]; // Creating Array with Array Literals
 
 let collections = new Array(1, 2, 3, 5, 6, 8);
+*/
+
+// Array Indices
+
+let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+console.log(colors.length);
+
+console.log(colors[0]);
+
+console.log(colors[4]);
+
+console.log(colors[colors.length - 1]);
+
+console.log(colors[213]);
