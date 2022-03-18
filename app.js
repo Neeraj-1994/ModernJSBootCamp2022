96479df487bf753f720137576a974a3579036889
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2022-03-18 11:46:38 +0600

    switch case implementation

--- app.js
+++ app.js
@@ -540,9 +540,42 @@ if (!(flavor === 'grape' || flavor === 'cherry')) {
 }
 */
 
+/*
 // Operator Precedence
 
 let x = 7;
 
 console.log(x === 7 || x === 3 && x > 10); // && Operator takes precedence over ||
+*/
 
+// The Switch Statement
+
+// Syntax: switch(statement) {
+// case 1: condition to be executed; break;
+// case 2: condition to be executed; break;
+// default: condition to be executed;
+// }
+
+let day = 3;
+
+switch(day) {
+    case 1: console.log('MONDAY'); break;
+    case 2: console.log('TUESDAY'); break;
+    case 3: console.log('WEDNESDAY'); break;
+    case 4: console.log('THURSDAY'); break;
+    case 5: console.log('FRIDAY'); break;
+    case 6: console.log('SATURDAY'); break;
+    case 7: console.log('SUNDAY'); break;
+    default: console.log('INVALID DAY!');
+}
+
+let emoji = 'sad face';
+
+switch(emoji) {
+    case 'sad face':
+    case 'happy face': console.log('yellow'); break;
+    case 'eggplant': console.log('purple'); break;
+    case 'lips':
+    case 'heart': console.log('red'); break;
+    default: console.log('no emoji!');
+}
