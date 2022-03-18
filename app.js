5fa11b76ff8a55ff5cbf90a3ddda461e3578845e
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2022-03-18 09:14:07 +0600

    nesting conditions for execution

--- app.js
+++ app.js
@@ -392,6 +392,7 @@ else if (rating === 1) {
 }
 */
 
+/*
 // Else
 
 // Syntax: if (statement 1) {
@@ -424,4 +425,21 @@ if (userScore >= highScore) {
     highScore = userScore;
 } else {
     console.log(`Good Game. Your score of ${userScore} did not beat the high score of ${highScore}`);
+}
+*/
+
+// Nesting Conditionals
+
+let password = 'kitty';
+
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid Password!");
+    }
+    else {
+        console.log("Password is long enough, but cannot contain spaces");
+    }
+}
+else {
+    console.log("Password must be longer!")
 }
