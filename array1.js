08368ee1480fe0bab9e0e7757466b185dc3c4b36
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2022-03-18 12:23:24 +0600

    create arrays both by literals and normal methods

---
+++ array1.js
@@ -0,0 +1,11 @@
+// Capture Collections of Data with Arrays
+
+// Creating Arrays
+
+let shoppingList = ['cheese', 'cereal', 'ice'];
+
+let lottoNumbers = [45, 12, 23, 25, 34];
+
+let myCollection = ['dog', 12, NaN, true, null]; // Creating Array with Array Literals
+
+let collections = new Array(1, 2, 3, 5, 6, 8);
