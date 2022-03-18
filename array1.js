8a9843c922a3432ac3794163d60361a0cfa3d701
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2022-03-18 13:41:13 +0600

    adding and changing elements of an array

--- array1.js
+++ array1.js
@@ -12,6 +12,7 @@ let myCollection = ['dog', 12, NaN, true, null]; // Creating Array with Array Li
 let collections = new Array(1, 2, 3, 5, 6, 8);
 */
 
+/*
 // Array Indices
 
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
@@ -25,3 +26,28 @@ console.log(colors[4]);
 console.log(colors[colors.length - 1]);
 
 console.log(colors[213]);
+*/
+
+// Modifying Arrays
+
+let shoppingList = ['Cheddar Cheese', '2% Milk'];
+
+shoppingList[1] = 'Whole Milk';
+
+console.log(shoppingList);
+
+shoppingList[2] = 'Ice Cream';
+
+console.log(shoppingList);
+
+shoppingList[shoppingList.length] = 'Tomatoes';
+
+console.log(shoppingList);
+
+shoppingList[shoppingList.length] = 'Potatoes';
+
+console.log(shoppingList);
+
+shoppingList[0] = 'Bread';
+
+console.log(shoppingList);
