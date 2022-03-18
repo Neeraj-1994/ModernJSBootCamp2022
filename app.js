72cafa58352836311112cfdc550f1300ecb733ae
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2022-03-18 11:06:18 +0600

    logical AND implementation

--- app.js
+++ app.js
@@ -446,6 +446,7 @@ else {
 }
 */
 
+/*
 // Truthy & Falsy Values
 
 let mystery = 1;
@@ -464,4 +465,29 @@ if (loggedInUser) {
 }
 else {
     console.log('PLEASE LOG IN!');
+}
+*/
+
+// Logical AND (&&)
+
+console.log(1 <= 4 && 'a' === 'a');
+
+console.log(9 > 10 && 9 >= 9);
+
+console.log('abc'.length === 3 && 1+1 === 4);
+
+let password = 'chickenGal';
+
+if (password.length >= 8 && password.indexOf(' ') === -1) {
+    console.log('VALID PASSWORD!');
+} else {
+    console.log('INVALID PASSWORD!');
+}
+
+let num = 3;
+
+if (num >= 1 && num <= 10) {
+    console.log('Number is between 1 and 10');
+} else {
+    console.log('Please guess a number between 1 and 10');
 }
