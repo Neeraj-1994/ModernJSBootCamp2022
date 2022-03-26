fb43b88b4ec4534df47d62bc916d76d0f44e3876
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2022-03-26 10:42:11 +0600

    creating objects and accessing properties

--- object1.js
+++ object1.js
@@ -1,3 +1,4 @@
+/*
 // Objects - The Core of JavaScript
 
 // Intro to Objects
@@ -9,3 +10,22 @@ const fitBitData = {
     workoutsThisWeek: '5 of 7',
     avgGoodSleep: '2:13'
 };
+*/
+
+// Creating Object Literals
+
+const fitBitData = {
+    totalSteps: 308727,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5755,
+    workoutsThisWeek: '5 of 7',
+    avgGoodSleep: '2:13'
+};
+
+console.log(fitBitData);
+
+console.log(fitBitData.totalMiles);
+
+console.log(fitBitData.avgCalorieBurn);
+
+console.log(fitBitData.workoutsThisWeek);
