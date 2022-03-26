003f59bdbf34fe5bb8a2b4efd5e8e939add67d8b
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2022-03-26 10:54:57 +0600

    accessing properties with square brackets

--- object1.js
+++ object1.js
@@ -12,6 +12,7 @@ const fitBitData = {
 };
 */
 
+/*
 // Creating Object Literals
 
 const fitBitData = {
@@ -29,3 +30,32 @@ console.log(fitBitData.totalMiles);
 console.log(fitBitData.avgCalorieBurn);
 
 console.log(fitBitData.workoutsThisWeek);
+*/
+
+// Accessing Object Properties
+
+const numbers = {
+    100: 'one hundred',
+    16: 'sixteen',
+    '76 trombones': 'great song!'
+};
+
+console.log(numbers[100]);
+
+console.log(numbers['100']); // When property names are not valid JS identifiers
+
+console.log(numbers['76 trombones']);
+
+const palette = {
+    red: '#eb4d4d',
+    yellow: '#f9ca24',
+    blue: '#30336b'
+}
+
+console.log(palette.red);
+
+console.log(palette['blue']);
+
+let mysteryColor = 'yellow';
+
+console.log(palette[mysteryColor]); // Dynamic value to access data from an object
