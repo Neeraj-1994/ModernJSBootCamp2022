1040b873a86535d62214ec626c2d7938f5a72cb9
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2022-03-26 10:31:40 +0600

    information stored in a specific way

---
+++ object1.js
@@ -0,0 +1,11 @@
+// Objects - The Core of JavaScript
+
+// Intro to Objects
+
+const fitBitData = {
+    totalSteps: 308727,
+    totalMiles: 11.7,
+    avgCalorieBurn: 5755,
+    workoutsThisWeek: '5 of 7',
+    avgGoodSleep: '2:13'
+};
